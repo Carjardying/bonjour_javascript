95a4ee1458ddeefb16b0eb75224ed90542087758
Carjardying <carolinejardy.pro@gmail.com>
2025-02-06 10:33:21 +0100

    étape 2.2 ok normalement

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-
-let message = "Bonjour !";
-let firstName = "Beyonce";
-
-message = `Bonjour ${firstName} !`;
-
-console.log(message);
+function sayHello() {
+  let message = "Bonjour !";
+  let firstName = "Beyonce";
+  message = `Bonjour ${firstName} !`;
+  console.log(message);
+  return message;
+}
+//let result = sayHello() /*à voir pourquoi le messsage console x2*/
+sayHello();
+// trouver moyen de faire plus petit et plus simple
